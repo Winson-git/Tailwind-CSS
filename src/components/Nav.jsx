e8dc06cda5739d7b99798dde288236b8a23dfba2
Winson Hong <winsonhong2001@gmail.com>
2024-07-07 01:26:37 +0800

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
     <header className='padding-x py-8 absolute z-10 w-full'>
         <nav className='flex justify-between items-center max-container'>
             <a href="/">
-                <img src={headerLogo} alt="Logo" width={130} height={29}/>
+                <img src={headerLogo} alt="Logo" width={130} height={29} className='m-0 w-[129px] h-[29px]' />
             </a>
             <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
                 {navLinks.map((item) => (
