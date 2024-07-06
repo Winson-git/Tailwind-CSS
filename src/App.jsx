593e657b9dce9125e52fc1474ad4ad04dafe8464
Winson Hong <winsonhong2001@gmail.com>
2024-07-07 02:14:27 +0800

    add - Hero Shoe Card

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Nav from "./components/Nav";
 const App = () => (
   <main className=" relative ">
     <Nav />
-    <section className="xl:padding-1 wide:padding-r padding-b">  {/* wide ; padding-r/b this is the new class we render and set in tailwind.config.js */}
+    <section className="xl:padding-l wide:padding-r padding-b">  {/* wide ; padding-r/b this is the new class we render and set in tailwind.config.js */}
       <Hero />
     </section>
     <section className="padding">
