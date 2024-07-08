9f192dc625033c8e4c7df3f831622dc97817283b
Winson Hong <winsonhong2001@gmail.com>
2024-07-08 21:14:14 +0800

    add - Subscribe section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const Button = ({ label, iconUrl, backgroundColor, textColor, borderColor }) => {
+const Button = ({ label, iconUrl, backgroundColor, textColor, borderColor, fullWidth }) => {
   return (
     <button
       className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
         ${backgroundColor ? backgroundColor : 'bg-coral-red'}
         ${textColor ? textColor : 'text-white'}
         ${borderColor ? borderColor : 'border-coral-red'}
+        ${fullWidth && 'w-full'}
         rounded-full`}
     >
         {label}
